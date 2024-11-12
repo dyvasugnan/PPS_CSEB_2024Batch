8e86e8e7a8901df28a0d5ee82272b59e7bda81a3
Sudha4884 <85675476+Sudha4884@users.noreply.github.com>
2024-11-12 19:28:48 +0530

    Add files via upload

---
+++ sample/f1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	//int number1 = 5;
+	//int number2 = 6789;
+	  float pi = 3.14159265;
+	  double f = 3.14159265;
+	  long double ld = 3.14159265;
+	 printf("%e\n",pi);
+	printf("%e\n",f);
+	printf("%e",ld);
+	return 0;
+
+}
