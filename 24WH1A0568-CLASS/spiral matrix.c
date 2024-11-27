4c9858ce6402a51b6c9135acc6c8dc5b3bc8b80a
24WH1A0568 <24wh1a0568@bvrithyderabad.edu.in>
2024-11-27 20:40:29 +0530

    Create spiral matrix.c

---
+++ 24WH1A0568-CLASS/spiral matrix.c
@@ -0,0 +1,21 @@
+//spiral matrix
+#include <stdio.h>
+int main() {
+    int a[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int i,j,top=0,bot=3,left=0,right=3;
+    while(top<=bot||left<=right){
+    for(i=left;i<=right;i++)
+    printf("%d ",a[top][i]);
+    top++;
+    for(i=top;i<=bot;i++)
+    printf("%d ",a[i][right]);
+    right--;
+    for(i=right;i>=left;i--)
+    printf("%d ",a[bot][i]);
+    bot--;
+    for(i=bot;i>=top;i--)
+    printf("%d ",a[i][left]);
+    left++;
+    }
+    return 0;
+}
