b714ef35bc570da7f79a99549a9b32d52e2a769b
24wh1a05a6 <24wh1a05a6@bvrithyderabad.edu.in>
2024-11-28 23:17:55 +0530

    Add files via upload

---
+++ 24wh1a05a6 _classroom/binary to decimal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main() 
+  {
+int i,num=0,n=5;
+char a[6]={1,0,0,0,0};
+for(i=0;i<n;i++) 
+{
+ if(a[i]==1)
+   
+   { 
+   (num=num+pow(2,n-1-i));   
+   }
+   printf("%d",num);
+   return 0;
+    }
+    }
+
+    
