1c2dc2c2085a55a5985cbda82a6ade15d76ab06a
24wh1a05a6 <24wh1a05a6@bvrithyderabad.edu.in>
2024-11-29 10:10:38 +0530

    Update binary to decimal.c

--- 24wh1a05a6 _classroom/binary to decimal.c
+++ 24wh1a05a6 _classroom/binary to decimal.c
@@ -3,7 +3,7 @@
 int main() 
   {
 int i,num=0,n=5;
-char a[6]={1,0,0,0,0};
+int a[6]={1,0,0,0,0};
 for(i=0;i<n;i++) 
 {
  if(a[i]==1)
@@ -11,9 +11,10 @@ for(i=0;i<n;i++)
    { 
    (num=num+pow(2,n-1-i));   
    }
+}
    printf("%d",num);
    return 0;
-    }
+    
     }
 
     
