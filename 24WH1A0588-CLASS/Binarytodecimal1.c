638573f2545afaae1ef6c3f1db5ea5820d1a3a2b
Minupala-Sharanya337 <24wh1a0588@bvrithyderabad.edu.in>
2024-11-29 10:08:09 +0530

    Update Binarytodecimal1.c

--- 24WH1A0588-CLASS/Binarytodecimal1.c
+++ 24WH1A0588-CLASS/Binarytodecimal1.c
@@ -3,7 +3,7 @@
 int main()                                 
   {                                        
 int i,num=0,n=5;                           
-char a[6]={1,0,1,0,0};                     
+int a[6]={1,0,1,0,0};                     
 for(i=0;i<n;i++)                           
 {                                          
  if(a[i]==1)                               
