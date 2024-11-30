e3fa8e280486b8355a1a634313affc46a1307f11
Ramya5a2 <24wh1a05a2@bvrithyderabad.edu.in>
2024-11-30 20:13:02 +0530

    Add files via upload

---
+++ 24wh1a05a2-Lab/majority_element_in_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    //majority element
+    int a[10]={1,2,2,2,2,8,34,2,1,2};
+    int n=sizeof(a)/sizeof(a[0]),max;
+    int count=1;
+    for(int i=0;i<n-1;i++){
+        count=1;
+        for(int j=i+1;j<n;j++){
+            if(a[i]==a[j])
+            count++;
+            if(count>n/2){
+                max=a[i];
+                printf("%d",max);
+                break;
+            }
+        }
+    } 
+    return 0;
+}
