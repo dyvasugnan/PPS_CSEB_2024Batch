d23ab822b3fade37f6333567c52377ef95ac7866
Ramya5a2 <24wh1a05a2@bvrithyderabad.edu.in>
2024-11-30 20:17:23 +0530

    Add files via upload

---
+++ 24wh1a05a2-class/playing_with_characters.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    char str[100];
+    char sen[100];
+  
+    scanf("%c",&ch);
+    printf("%c\n",ch); 
+     
+   scanf("%s",str);
+   printf("%s",str);
+   
+   scanf("%[^@]s",sen);//it reads upto @ value
+   printf("%s",sen);
+
+return 0;
+}
