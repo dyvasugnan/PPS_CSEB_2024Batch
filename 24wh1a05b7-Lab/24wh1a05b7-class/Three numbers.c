99010047dad4f967d3e77d1d604c49b239faf238
akhila2203 <24wh1a05B7@bvrithyderabad.edu.in>
2024-12-03 21:07:50 +0530

    Add files via upload

---
+++ 24wh1a05b7-Lab/24wh1a05b7-class/Three numbers.c
@@ -0,0 +1,17 @@
+//sum and average of three numbers
+#include<stdio.h>
+int main()
+{
+int num1=2;
+int num2=3;
+int num3=4;
+int total=num1+num2+num3;
+int average=total/3;
+printf("%d\n", total);
+printf("%d", average);
+  return 0;
+}   
+   
+    
+
+    
