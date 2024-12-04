2f9f0c6223c4a9d64587374aee9764b7cdc0f793
Manaswini-2512 <24wh1a05a1@bvrithyderabad.edu.in>
2024-12-04 18:49:43 +0530

    Add files via upload

---
+++ 24wh1a05a1-class/conditionalstatement.c
@@ -0,0 +1,38 @@
+//switch case problem in hacker rank
+#include<stdio.h>
+int main(){
+    int n;
+    //enter any no
+    scanf("%d",&n);
+    switch(n){
+       case 1:
+       printf("one");
+       break;
+        case 2:
+       printf("two");
+       break;
+        case 3:
+       printf("three");
+       break;
+        case 4:
+       printf("four");
+       break;
+        case 5:
+       printf("five");
+       break;
+        case 7:
+       printf("seven");
+       break;
+        case 8:
+       printf("eight");
+       break;
+        case 9:
+       printf("nine");
+       break;
+        default:
+        printf("Greater than 9");
+       
+    }
+ return 0;   
+}
+ 
