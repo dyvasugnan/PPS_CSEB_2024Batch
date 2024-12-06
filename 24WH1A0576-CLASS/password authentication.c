077e016b6ccaae9d7e24cb4d36f735730dce0d11
Hasini401 <24wh1a0576@bvrithyderabad.edu.in>
2024-12-06 10:56:11 +0530

    Update password authentication.c

--- 24WH1A0576-CLASS/password authentication.c
+++ 24WH1A0576-CLASS/password authentication.c
@@ -5,11 +5,11 @@ int main(){
 char originalpin[10]="hasi24";
 char enterpin[10];
 int attempt=0;//starts from 0th attempt
-int retry=3;//no. of times to  the pin
+int retry=3;//no. of times to enter the pin
 while(attempt<retry){
    printf("enter your pin:\n");
    scanf("%s",&enterpin);
-if(strcmp(originalpin,enterpin)==0){
+if(strcmp(originalpin,enterpin)==0){//comparision of string
     printf("password authentication successful\n");
     break;
 }
