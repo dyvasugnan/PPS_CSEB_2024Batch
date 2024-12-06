144c8e8fbb8764d3e981fb09b48776c6c5d7a359
Pranathi-19 <24wh1a0584@bvrithyderabad.edu.in>
2024-12-06 10:17:19 +0530

    Add files via upload

---
+++ 24WH1A0584-CLASS/functions_fibonacci_upto_nterms.c
@@ -0,0 +1,21 @@
+// fibonacci series upto n terms in function //
+#include<stdio.h>
+int fibonacci_upto_n(int);
+int main(){
+    int n;
+    printf("Enter the number of terms:\n");
+    scanf("%d",&n);
+    int fib=fibonacci_upto_n(n);
+    return 0;
+}
+int  fibonacci_upto_n(int n){
+    int t0=0,t1=1,i,nextterm;
+    printf ("%d",t0);
+    printf ("%5d",t1);
+    for(i=1;i<=n;i++){
+        nextterm=t0+t1;
+        printf("%5d",nextterm);
+        t0=t1;
+        t1=nextterm;
+    }
+}
