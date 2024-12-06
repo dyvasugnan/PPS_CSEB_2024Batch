fd6ad1b129433575fc986d508f217de674c4efc8
Hasini401 <24wh1a0576@bvrithyderabad.edu.in>
2024-12-06 10:52:01 +0530

    Create password authentication.c

---
+++ 24WH1A0576-CLASS/password authentication.c
@@ -0,0 +1,27 @@
+//password authentication
+#include<stdio.h>
+#include<string.h>
+int main(){
+char originalpin[10]="hasi24";
+char enterpin[10];
+int attempt=0;//starts from 0th attempt
+int retry=3;//no. of times to  the pin
+while(attempt<retry){
+   printf("enter your pin:\n");
+   scanf("%s",&enterpin);
+if(strcmp(originalpin,enterpin)==0){
+    printf("password authentication successful\n");
+    break;
+}
+    else{
+        attempt++;
+        if (attempt<retry){
+            printf("try again\n");
+        }
+        else {
+            printf("youve reached maximum trails\n");
+        }
+    }
+    }
+    return 0;
+}
