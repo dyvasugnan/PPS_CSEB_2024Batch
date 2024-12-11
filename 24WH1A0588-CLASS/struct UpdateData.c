e53637066a9eeb95ae42b17fc055fa9e51828778
Minupala-Sharanya337 <24wh1a0588@bvrithyderabad.edu.in>
2024-12-11 23:39:11 +0530

    Add files via upload

---
+++ 24WH1A0588-CLASS/struct UpdateData.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<ctype.h>
+struct student{
+    char name[50];
+    int age;
+    }s1;
+    struct student UpdateData(struct student);
+    int main(){
+        printf("Enter the students name: \n");
+        scanf("%s",s1.name);
+        printf("Enter the student's age :\n");
+        scanf("%d",&s1.age);
+        struct student stu=UpdateData(s1);
+        printf("%s\n",stu.name);
+        printf("%d\n",stu.age);
+        return 0;
+    }
+    
+    struct student UpdateData( struct student s1){
+        s1.name[0]=toupper (s1.name[0]);
+        s1.age+=1;
+        return s1;
+    }
