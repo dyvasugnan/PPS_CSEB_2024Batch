3695459cfcb0eec50cc7fd752f390a701605cb3c
Sanjana-5B1 <24wh1a05b1@bvrithyderabad.edu.in>
2024-12-12 04:41:58 -0800

    Add files via upload

---
+++ 24WH1A05B1-LAB/type_casting.c
@@ -0,0 +1,12 @@
+// write a program to convert from one type to another type 
+#include<stdio.h>
+int main()
+{
+int a=6,b=7,c=3,sum=a+b+c;
+float avg=(float)(sum/3.0);
+printf("%f \n",avg);
+int x=avg;
+printf("%d",x);
+return 0;
+}
+
