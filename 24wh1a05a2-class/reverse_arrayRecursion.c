bd603b69c32ac6a8942a5bfd7f97a61c2e3cacba
Ramya5a2 <24wh1a05a2@bvrithyderabad.edu.in>
2024-12-15 15:15:26 +0530

    Add files via upload

---
+++ 24wh1a05a2-class/reverse_arrayRecursion.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+int arr(int n,int a[n],int i) {
+	if(n==0)
+	   return a[n];
+	printf("%d\t",a[i]);
+    return arr(n-1,a,i-1);
+  }
+
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d" ,&a[i]); 
+    i=n-1;
+    arr(n, a,i);
+    
+    return 0;
+}
+
+
+
