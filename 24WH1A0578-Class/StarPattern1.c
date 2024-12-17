cda961659f21ab5bdca6419df03f5f08db112b2f
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2024-12-17 11:43:06 +0530

    Create StarPattern1.c

---
+++ 24WH1A0578-Class/StarPattern1.c
@@ -0,0 +1,14 @@
+//star patterns
+#include <stdio.h>
+int main() {
+    int i,j;
+    int row=5;
+    int col=5;
+    for(row=1;row<=5;row++){
+        for(col=1;col<=5;col++){
+            printf("%d ",col);
+        }
+    printf("\n ");
+    }
+    return 0;
+}
