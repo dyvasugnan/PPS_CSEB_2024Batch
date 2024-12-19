83e89d2af64879f33833cabfab1ba5467a71b727
24wh1a05a5 <24wh1a05a5@bvrithyderabad.edu.in>
2024-12-19 20:36:07 +0530

    Add files via upload

---
+++ 24wh1a05a5_lab/gcd.c
@@ -0,0 +1,24 @@
+nclude<stdio.h>
+int gcd(int,int);
+int main(){
+
+int num1,num2;
+scanf("%d,%d",&num1,&num2);
+
+ int res = gcd(num1,num2);
+printf("%d",res);
+
+}
+int gcd(int x,int y){
+if(x<y)
+return gcd(y,x);
+if(y == 0)
+return x;
+else
+return gcd(y,x%y);
+
+
+}
+~                                                                               
+"GCD.c" 22L, 260C                                             1,1           All
+
