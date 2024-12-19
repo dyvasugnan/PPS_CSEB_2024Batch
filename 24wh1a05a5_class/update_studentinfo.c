7a21f3ab8ca5400ec66d2b0cbfe7947561d5c20f
24wh1a05a5 <24wh1a05a5@bvrithyderabad.edu.in>
2024-12-19 20:39:21 +0530

    Add files via upload

---
+++ 24wh1a05a5_class/update_studentinfo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+struct student{
+char name[20];
+int age;
+
+};
+struct student update_student_info(struct student);
+ int main(){
+
+  struct student s;
+ scanf("%s",s.name);
+scanf("%d",&s.age);
+s= update_student_info(s);
+printf("%s \n",s.name);
+printf("%d",s.age);
+ 
+ return 0;
+}
+struct student update_student_info(struct student stu){
+for(int i=0;stu.name[i]!='\0';i++){
+if(i ==0)
+ stu.name[i]=stu.name[i]-32;}
+  stu.age =stu. age+1;
+   return stu;
+
+}
