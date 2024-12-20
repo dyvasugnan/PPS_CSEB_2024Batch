c4f8cc3eb141e727830c0166a587b9e80de48b93
24WH1A05B9 <24wh1a05b9@bvrithyderabad.edu.in>
2024-12-20 20:11:34 +0530

    Add files via upload

---
+++ 24wh1a05b9 -Lab/factorial_recursion.c
@@ -0,0 +1,21 @@
+//factorial of any number using recursion
+#include<stdio.h>
+ int factorial(int); 
+   int main(){
+    int num;
+     printf("enter the number");
+     scanf("%d",&num);
+    int fact=factorial(num);
+        printf("\n%d",fact);
+     return 0;
+      }
+      int factorial(int num)
+        {
+        if(num==1)
+         return 1;
+         return num*factorial(num-1);
+        }
+         
+         
+
+
