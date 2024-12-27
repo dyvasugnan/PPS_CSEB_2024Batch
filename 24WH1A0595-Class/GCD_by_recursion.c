5b44d0a6b061f020be933b2984f38f5ccd1050ca
lasyasriyerramsetti <24wh1a0595@bvrithyderabad.edu.in>
2024-12-27 06:58:55 +0530

    Create GCD_by_recursion.c

---
+++ 24WH1A0595-Class/GCD_by_recursion.c
@@ -0,0 +1,19 @@
+//gcd using recursion
+#include <stdio.h>
+int gcd(int,int);
+int main() {
+   int n1,n2;
+   scanf("%d%d",&n1,&n2);
+   printf("%d", gcd(n1,n2));
+    return 0;
+}
+int gcd(int num1,int num2){
+  if (num1==0)
+    return num2;
+  if (num2==0)
+    return num1;
+  if(num2>num1 && num1!=0)
+    return gcd(num1,num2%num1);
+  if(num1>num2 && num2!=0)
+    return gcd(num2,num1%num2);
+}
