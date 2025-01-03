baad7d4dbce2fa100f941df866fefa829cf3e7d9
24WH1A05A7 <24wh1a05a7@bvrithyderabad.edu.in>
2025-01-03 22:47:26 +0530

    revofarray_hkrk.c

---
+++ 24WH1A05A7-Class/revofarray_hkrk.c
@@ -0,0 +1,24 @@
+//reverse of an array without recursion
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    int start = 0, end = num - 1, temp;
+    while(start < end){
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
