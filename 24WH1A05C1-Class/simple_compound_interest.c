7a0bb4e54def7831b19f808d75dd4688716c0a33
24WH1A05C1 <24wh1a05c1@bvrithyderabad.edu.in>
2025-01-03 23:07:43 +0530

    Add files via upload

---
+++ 24WH1A05C1-Class/simple_compound_interest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int principle =1000;
+     int rate=10;
+     int time=5;
+int simple_interset=principle*rate*time/100;
+   printf("simple interset :%d", simple_interest);
+ return 0;
+}
---
+++ 24WH1A05C1-Class/type_conversion.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=4,b=3,c=8;
+    int sum=a+b+c;
+    float avg=15/3;
+    avg = (float)(sum)/3;
+    printf("%f",avg);
+    return 0;
+    }
