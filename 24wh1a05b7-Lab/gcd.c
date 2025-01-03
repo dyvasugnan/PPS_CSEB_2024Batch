dbc1b42fc9b00bbbe59762eecad0d20fdff731d3
akhila2203 <24wh1a05B7@bvrithyderabad.edu.in>
2025-01-03 17:41:57 +0530

    Add files via upload

---
+++ 24wh1a05b7-Lab/gcd.c
@@ -0,0 +1,18 @@
+//finding gcd usind recursion
+#include<stdio.h>
+int gcd(int a,int b){
+     if(b==0){
+return a;
+}
+else {
+    return gcd(b,a%b);
+}
+}
+   int main(){
+int num1,num2;
+printf("enter the two numbers ");
+scanf("%d %d",&num1,&num2);
+     int result=gcd(num1,num2);
+printf("gcd of %d and %d is :%d\n",num1,num2,result);
+      return 0;
+}
