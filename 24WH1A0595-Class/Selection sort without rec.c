ca303041fe035cdf111e172a4eb05c5c5542c027
lasyasriyerramsetti <24wh1a0595@bvrithyderabad.edu.in>
2025-01-03 20:33:59 +0530

    Create Selection sort without rec.c

---
+++ 24WH1A0595-Class/Selection sort without rec.c
@@ -0,0 +1,48 @@
+//selection sort without recursion
+#include<stdio.h>
+void swap(int*, int*);
+void selectionSort(int [],int);
+void printArr(int [],int);
+
+int main(){
+ int arr[10];
+ int n,i;
+ printf("Enter no of elements:");
+ scanf("%d",&n);
+ printf("Enter the elements into array:");
+ for(i=0;i<n;i++)
+   scanf("%d",&arr[i]);
+ printf("Before sorting: \n");
+ printArr(arr,n); 
+ selectionSort(arr,n);
+ printf("\nAfter sorting: \n");
+ printArr(arr,n);
+return 0;
+}
+
+void selectionSort(int a[], int n){
+    int i,range=0, min;
+  for(range=0;range<n-1;range++){
+    min=range;
+      if (range == n)
+       return;
+    for(i=range+1;i<n;i++){
+        if(a[i]<a[min]){
+           min=i;
+        }
+    }
+    swap(&a[min],&a[range]);
+  }
+}
+
+void swap(int *n1, int *n2){
+    int temp=*n1;
+    *n1=*n2;
+    *n2= temp;
+}
+
+void printArr(int a[],int n){
+    int i;
+    for(i=0;i<n;i++)
+   printf("%d   ",a[i]);
+}
