d19f93c60c803ef63de1894d8b4f70d7140f6132
SRIJA050 <asrija1702@gmail.com>
2025-01-04 20:59:31 +0530

    DisplayrevArray_poster.c

---
+++ 24WH1A0592-lab/DisplayrevArray_poster.c
@@ -0,0 +1,14 @@
+//display array by using pointers
+#include<stdio.h>
+int main(){
+int arr[10],i,n;
+printf("enter size of array:");
+scanf("%d",&n);
+printf("enter arrayelements\n" );
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+printf("print the array elements:\n");
+for(i=n-1;i>=0;i--)
+printf("%4d",*(arr+i));
+return 0;
+}
