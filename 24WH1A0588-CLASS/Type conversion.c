896c3fa0a6f1f7aace5e00425b086b9793130361
Minupala-Sharanya337 <24wh1a0588@bvrithyderabad.edu.in>
2025-01-04 14:47:07 +0530

    Create Type conversion.c

---
+++ 24WH1A0588-CLASS/Type conversion.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+}
+
+int x = 10;
+
+char y = 'a';
+
+x = x + y;
+
+float z = x + 1.0;
+
+printf("Implicit type conversion\n");
+
+printf("x = %d, z = %f", x, z);
+
+return 0;
+
+//explicit type conversion
+
+#include<stdio.h>
+
+int main()
+
+{
+
+}
+
+
+
+double x = 1.2;
+
+int sum = (int)x + 1;
+
+printf("sum = %d",sum);
+return 0;
