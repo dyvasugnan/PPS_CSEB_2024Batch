718ee22ff3dd256bb32a152109fc0cf26b313315
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-04 10:31:53 +0530

    Arithmetic_operators.c

---
+++ 24WH1A0578-Class/Arithmetic_operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int a = 25, b = 5;
+int num = 10;
+   printf("Arithmetic operators\n");
+   printf("a + b = %d\n", a + b);
+   printf("a - b = %d\n", a - b);
+   printf("a * b = %d\n", a * b);
+  printf("a / b = %d\n", a / b);
+return 0;
+}
