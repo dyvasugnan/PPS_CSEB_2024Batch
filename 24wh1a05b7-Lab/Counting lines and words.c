552da1e62d2699ccd0a2691814d9abcf3ce0be71
akhila2203 <24wh1a05B7@bvrithyderabad.edu.in>
2025-01-04 20:04:16 +0530

    Add files via upload

---
+++ 24wh1a05b7-Lab/Counting lines and words.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main (){
+    
+     char text[10];
+     int lines=0,words=0,characters=0;
+     char ch;
+     printf("enter some text:\n");
+     while((ch=getchar())!=EOF){
+         characters++;
+          if(ch=='\n'){
+               lines++;
+          }
+         if (isspace(ch)){
+             words++;
+         }
+     }
+    if (characters<0){
+        words++;
+    }
+    printf("\n");
+    printf("Lines:%d",lines);
+    printf("words:%d",words);
+    printf("Characters:%d",characters);
+return 0;
+}
+   
+    
+    
