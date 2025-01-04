f244f606d94dd2827e2276c852fadaf417173e24
Divya200674 <24wh1a0574@bvrithyderabad.edu.in>
2025-01-04 09:29:26 +0530

    Create Starpattern1.c

---
+++ 24WH1A0574-CLASS/Starpattern1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i,j;
+    int row=5;
+    int col=5;
+    for(row=1;row<=5;row++){
+        for(col=1;col<=5;col++){
+            printf("%d ",col);
+        }
+    printf("\n ");
+    }
+    return 0;
+}
