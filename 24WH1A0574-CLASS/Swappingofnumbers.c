fac90ccc87d4fd448d5b5d69fc8431fefe8f5d8d
Divya200674 <24wh1a0574@bvrithyderabad.edu.in>
2025-01-04 09:17:30 +0530

    Create Swappingofnumbers.c

---
+++ 24WH1A0574-CLASS/Swappingofnumbers.c
@@ -0,0 +1,20 @@
+//swapping of two numbers
+#include<stdio.h>
+void swap2num(int,int);
+
+int main(){
+int n1,n2;
+printf("enter value of number 1:\n");
+scanf("%d",&n1);
+printf("enter value of number 2:\n");
+scanf("%d",&n2);
+printf("before swapping two numbers the values are: n1=%d,n2=%d\n",n1,n2);
+swap2num(n1,n2);
+return 0;
+}
+void swap2num(int a,int b){
+int temp=a;
+a=b;
+b=temp;
+printf("after swapping two numbers the values are: n1=%d,n2=%d\n",a,b);
+}
