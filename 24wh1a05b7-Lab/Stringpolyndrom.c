7f2f947314db1f766c52abe365293994cb285e65
akhila2203 <24wh1a05B7@bvrithyderabad.edu.in>
2025-01-04 19:48:03 +0530

    Add files via upload

---
+++ 24wh1a05b7-Lab/Stringpolyndrom.c
@@ -0,0 +1,17 @@
+//string polindrom
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+     char str[]={"racecar"};
+     int l=0;
+     int h =strlen(str)-1;
+     while(h>1){
+         if(str[l++]!=str[h--]){
+             printf("%s is not a polindrome\n",str);
+             return 0;
+         }
+     }
+    printf("%s is palindrome",str);
+    return 0;
+}
