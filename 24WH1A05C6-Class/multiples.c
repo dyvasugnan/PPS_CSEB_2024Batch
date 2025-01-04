ab63362c921cfa8abdd34aef9fd41e64478c75d0
PravalikaGundeboina <24wh1a05c6@bvrithyderabad.edu.in>
2025-01-04 21:39:13 +0530

    Add files via upload

---
+++ 24WH1A05C6-Class/multiples.c
@@ -0,0 +1,10 @@
+// multiples
+#include<stdio.h>
+int main(){
+   int num,i;
+      printf("enter number\n");
+       scanf("%d",&num);
+        printf("multiples of %d\n",num);
+   for(i=1;i<=10;i++)
+       printf("%d x %d = %d\n",num,i,num*i);
+        return 0;}
