deccfca4fe5251eef2cf7d2734a94d8d31d105aa
PravalikaGundeboina <24wh1a05c6@bvrithyderabad.edu.in>
2025-01-04 21:26:31 +0530

    Add files via upload

---
+++ 24WH1A05C6-Class/max_min .c
@@ -0,0 +1,31 @@
+// max and min from 3 numbers
+#include<stdio.h>
+int main(){
+  int n1,n2,n3;
+  int max,min;
+  printf("Enter 3 numbers\n");
+  scanf("%d%d%d",&n1,&n2,&n3);
+  printf("n1=%d n2=%d n3=%d\n",n1,n2,n3);
+  if(n1>n2&&n1>n3)
+      max=n1;
+      
+     else if(n2>n1&&n2>n3)
+       max=n2;
+  else
+       max=n3;
+       printf("max = %d\n",max);
+
+
+
+  if(n1<n2)
+     if(n1<n3)
+        min = n1;
+  else
+     min = n3;
+  if(n2<n1)
+     if(n2<n3)
+        min = n2;
+  else
+     min = n3;
+     printf("min = %d\n",min);
+       return 0;}       
