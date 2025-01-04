dc6a63576fda11d4f77d0dd7125b59b4f2d6473b
Divya200674 <24wh1a0574@bvrithyderabad.edu.in>
2025-01-04 09:35:43 +0530

    Create Sumofnaturalnum.c

---
+++ 24WH1A0574-LAB/Sumofnaturalnum.c
@@ -0,0 +1,17 @@
+//sum of n natural numbers
+#include<stdio.h>
+int sumofnatnum(int);
+int main(){
+int num,sum;
+printf("enter value of n:\n");
+scanf("%d",&num);
+sum= sumofnatnum(num);
+printf("sum:%d\n",sum);
+return 0;
+}
+int sumofnatnum(int x){
+int sum=0,i;
+for(i=1;i<=x;i++)
+sum+=i;
+return sum;
+}
