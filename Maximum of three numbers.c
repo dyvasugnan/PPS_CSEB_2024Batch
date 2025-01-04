00d0f88c5dee19748821eb01e339b40e5c561860
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-04 10:39:03 +0530

    Maximum of three numbers.c

---
+++ Maximum of three numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+int n1, n2, n3;
+printf("Enter three numbers: ");
+scanf("%d %d %d", &n1, &n2, &n3);
+     if (n1>n2){
+
+else{
+
+}
+
+if(n1>n3){
+
+    printf("%d is the largest number", n1);
+
+else{
+
+   printf("%d is the largest number", n3);
+
+if(n2>n3){
+
+   printf("%d is the largest number", n2);
+
+else{
+
+   printf("%d is the largest number",n3);
+}
+  return 0;
+}
