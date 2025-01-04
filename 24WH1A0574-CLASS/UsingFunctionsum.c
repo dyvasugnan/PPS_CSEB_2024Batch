e46cfb53e3798d6beb1bd005b43702cab9c5c0ff
Divya200674 <24wh1a0574@bvrithyderabad.edu.in>
2025-01-04 09:26:22 +0530

    Create  UsingFunctionsum.c

---
+++ 24WH1A0574-CLASS/UsingFunctionsum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sum(int,int);
+int main(){
+int n1,n2;
+printf("enter value of number 1:");
+scanf("%d",&n1);
+printf("enter value of number 2:");
+scanf("%d",&n2);
+sum(n1,n2); //calling of value
+return 0;
+}
+int sum(int a,int b){
+int result;
+result=a+b;
+printf("sum of two numbers is:%d",result);
+return result;
+}
