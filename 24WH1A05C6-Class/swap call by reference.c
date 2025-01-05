9e6ec920f38c491c4af7a866cebb606a0fa60543
PravalikaGundeboina <24wh1a05c6@bvrithyderabad.edu.in>
2025-01-05 22:53:34 +0530

    Add files via upload

---
+++ 24WH1A05C6-Class/swap call by reference.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main(){
+int n1,n2;
+printf("Enter the 2 numbers : ");
+scanf("%d%d",&n1,&n2);
+ swap(&n1,&n2);
+ printf("n1=%d n2=%d", n1,n2);
+return 0;
+}
+void swap(int *ptr1,int *ptr2){
+ int temp;
+ temp=*(ptr1);
+ *(ptr1)=*(ptr2);
+ *(ptr2)=temp;}
