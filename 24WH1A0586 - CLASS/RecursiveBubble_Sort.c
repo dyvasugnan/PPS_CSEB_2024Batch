a6f6a027cdf069ee1eb6de3805eeef36a4b52856
24WH1A0586 <24wh1a0586@bvrithyderabad.edu.in>
2025-01-05 15:39:49 +0530

    Create RecursiveBubble_Sort.c

---
+++ 24WH1A0586 - CLASS/RecursiveBubble_Sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+void bubbleSort(int a[], int n);
+void swap(int *a, int *b);
+void printArray(int a[], int n);
+
+int main() {
+   int n, i;
+   printf("Enter the size: ");
+   scanf("%d", &n);
+   int a[n];
+   printf("Enter the array elements: ");
+   for(i = 0; i < n; i++){
+       scanf("%d", &a[i]);
+   }
+   printf("Array before sorting: ");
+   printArray(a, n);
+   bubbleSort(a, n);
+   printf("The Sorted Array: ");
+   printArray(a, n);
+   return 0;
+}
+
+void bubbleSort(int a[], int n) {
+ 
+    if (n == 1) {
+        return;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            int temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+    }
+
+    bubbleSort(a, n - 1);
+}
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void printArray(int a[],int n){
+    int i;
+       for(i=0;i<n;i++){
+       printf(" %d",a[i]);
+   }printf("\n");
+}
