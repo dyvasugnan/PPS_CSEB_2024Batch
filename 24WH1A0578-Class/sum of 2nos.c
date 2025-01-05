11538ffe50ea0cb7b8c37dfea00eea9c2dfb4dba
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-05 21:11:41 +0530

    sum of 2nos.c

---
+++ 24WH1A0578-Class/sum of 2nos.c
@@ -0,0 +1,12 @@
+//sum of two numbers
+#include <stdio.h>
+int main(){
+    int n1,n2,sum,avg;
+    printf("enter two number\n");
+    scanf("%d%d",&n1,&n2);
+    sum=n1 + n2;
+    printf("%d",sum);
+    
+    return 0;
+}
+    
