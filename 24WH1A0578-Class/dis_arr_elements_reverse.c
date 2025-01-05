fc5847fb5b0021a56eb45dbe78cfa276635ed346
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-05 20:39:05 +0530

    dis_arr_elements_reverse.c

---
+++ 24WH1A0578-Class/dis_arr_elements_reverse.c
@@ -0,0 +1,15 @@
+//reading elements into the array using a pointer display reverse order
+#include <stdio.h>
+int main() {
+    int a[5],i;
+    int *ptr=a;
+    printf("enter numbers");
+   for(i=0;i<5;i++){
+       scanf("%d",ptr+i);
+    }
+   printf("reverse order: ");
+    for(int i= 5-1;i>=0;i--){
+        printf("%d  ",*(ptr+i)); 
+    }
+    return 0;
+}
