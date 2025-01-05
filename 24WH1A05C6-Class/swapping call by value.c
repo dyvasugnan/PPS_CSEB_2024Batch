e397c5100e9bd3115e5f5699c4b9423c8c740b5b
PravalikaGundeboina <24wh1a05c6@bvrithyderabad.edu.in>
2025-01-05 22:58:08 +0530

    Add files via upload

---
+++ 24WH1A05C6-Class/swapping call by value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int ,int );
+int main(){
+int n1,n2;
+printf("Enter the 2 numbers : ");
+scanf("%d%d",&n1,&n2);
+ swap(n1,n2);
+ 
+return 0;
+}
+void swap(int n1,int n2){
+ int temp;
+ temp=n1;
+ n1=n2;
+ n2=temp;
+printf("n1=%d n2=%d", n1,n2);
+ }
