4c59e1ce5eecdd24e72868110bac90767aa2d432
PravalikaGundeboina <24wh1a05c6@bvrithyderabad.edu.in>
2025-01-05 22:33:28 +0530

    Add files via upload

---
+++ 24WH1A05C6-Class/class awarded for marks.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int marks;
+printf("\n Enter Marks between 0-100:");
+scanf("%d", & marks);
+if (marks >= 70)
+{
+printf("\n You got Distinction");
+}
+else if (marks >= 60)
+{
+printf("\n You got First Class");
+}
+else if (marks >= 40)
+{
+printf("\n You got Second Class");
+}
+else
+{
+printf("\n You got Failed");}
+return 0;}
