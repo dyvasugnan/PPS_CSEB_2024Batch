942891145adc554527b93731cfe553ac132131bb
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-05 20:52:45 +0530

    string_polindrome.c

---
+++ 24WH1A0578-Lab/string_polindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdbool.h>
+bool isPalindrome(char []);
+int main(){
+   char str[]="madam";
+   if(isPalindrome(str))
+    printf("Palindrome");
+   else
+    printf("not Palindrome");
+    return 0;
+}
+bool isPalindrome(char s[]){
+    int i=0;
+    int len=strlen(s);
+    for (i=0;i<len/2;i++){
+        if(s[i]!=s[len-i-1])
+        return false;
+    }
+    return true;
+}
