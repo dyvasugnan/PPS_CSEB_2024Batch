57faa4e8d26e5bef97c25a3d04d3401304f2e5f5
Srilatha-72 <24wh1a0578@bvrithyderabad.edu.in>
2025-01-05 13:17:04 +0530

    letter_pattern.c

---
+++ 24WH1A0578-Lab/letter_pattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+      int r,c,n=5;
+      char ch='A';
+      for(r=1;r<=n;r++){
+         for(c=1;c<=n;c++)
+               printf("%c  ",ch++);
+             printf("\n");
+             }
+      return 0;
+      
+      
+       }
